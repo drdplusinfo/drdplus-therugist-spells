5f56c87645e553c5766d0d09982d5e36e31e1e5b
Jaroslav Týc <mail@jaroslavtyc.com>
2017-05-01 10:27:12 +0200

    Addition by realms increasing and decreasing tested

--- DrdPlus/Tests/Theurgist/Formulas/CastingParameters/AdditionByRealmsTest.php
+++ DrdPlus/Tests/Theurgist/Formulas/CastingParameters/AdditionByRealmsTest.php
@@ -14,9 +14,14 @@ class AdditionByRealmsTest extends TestWithMockery
         $additionByRealms = new AdditionByRealms('123');
         self::assertSame(123, $additionByRealms->getDefaultAddition());
         self::assertSame(1, $additionByRealms->getRealmIncrementPerAddition());
+        self::assertSame($additionByRealms->getDefaultAddition(), $additionByRealms->getCurrentAddition());
+        self::assertSame($additionByRealms->getRealmIncrementPerAddition(), $additionByRealms->getCurrentRealmIncrement());
+
         $sameAdditionByRealms = new AdditionByRealms('1=123');
         self::assertSame(123, $sameAdditionByRealms->getDefaultAddition());
         self::assertSame(1, $sameAdditionByRealms->getRealmIncrementPerAddition());
+        self::assertSame($sameAdditionByRealms->getDefaultAddition(), $sameAdditionByRealms->getCurrentAddition());
+        self::assertSame($sameAdditionByRealms->getRealmIncrementPerAddition(), $sameAdditionByRealms->getCurrentRealmIncrement());
     }
 
     /**
@@ -27,6 +32,48 @@ class AdditionByRealmsTest extends TestWithMockery
         $additionByRealms = new AdditionByRealms('456=789');
         self::assertSame(789, $additionByRealms->getDefaultAddition());
         self::assertSame(456, $additionByRealms->getRealmIncrementPerAddition());
+        self::assertSame($additionByRealms->getDefaultAddition(), $additionByRealms->getCurrentAddition());
+        self::assertSame($additionByRealms->getRealmIncrementPerAddition(), $additionByRealms->getCurrentRealmIncrement());
+    }
+
+    /**
+     * @test
+     */
+    public function I_can_create_it_with_custom_current_addition()
+    {
+        $additionByRealms = new AdditionByRealms('2=3', 7);
+        self::assertSame(3, $additionByRealms->getDefaultAddition());
+        self::assertSame(2, $additionByRealms->getRealmIncrementPerAddition());
+        self::assertSame(7, $additionByRealms->getCurrentAddition());
+        self::assertSame(5 /* 7 / 3 * 2, round up */, $additionByRealms->getCurrentRealmIncrement());
+    }
+
+    /**
+     * @test
+     */
+    public function I_can_increase_current_addition()
+    {
+        $additionByRealms = new AdditionByRealms(5);
+        self::assertSame(5, $additionByRealms->getDefaultAddition());
+        self::assertSame(5, $additionByRealms->getCurrentAddition());
+        $increased = $additionByRealms->add(3);
+        self::assertSame(5, $additionByRealms->getCurrentAddition());
+        self::assertNotSame($additionByRealms, $increased);
+        self::assertSame(8, $increased->getValue());
+    }
+
+    /**
+     * @test
+     */
+    public function I_can_decrease_current_addition()
+    {
+        $additionByRealms = new AdditionByRealms(5);
+        self::assertSame(5, $additionByRealms->getDefaultAddition());
+        self::assertSame(5, $additionByRealms->getCurrentAddition());
+        $increased = $additionByRealms->sub(7);
+        self::assertSame(5, $additionByRealms->getCurrentAddition());
+        self::assertNotSame($additionByRealms, $increased);
+        self::assertSame(-2, $increased->getValue());
     }
 
     /**
